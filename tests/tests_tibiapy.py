4cf70ec52026016d9721c308ac661cd6e1c8ab4e
Allan Galarza <allan.galarza@gmail.com>
2018-08-08 06:36:02 -0700

    Added example unittest

---
+++ tests/tests_tibiapy.py
@@ -0,0 +1,14 @@
+import unittest
+
+from tibiapy import Guild
+
+
+class TestTibiaPy(unittest.TestCase):
+    def testGuilds(self):
+        with open("resources/GuildActive.html") as f:
+            content = f.read()
+        guild = Guild.from_content(content)
+        self.assertTrue(guild.active)
+        self.assertIsNotNone(guild.description)
+        self.assertTrue(guild.members)
+        self.assertIsInstance(guild.members[0].level, int)
