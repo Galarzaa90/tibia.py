9920191aa47a8e5e8fc55f9c93f5f2940213ada8
Allan Galarza <allan.galarza@gmail.com>
2018-12-20 10:59:39 -0700

    Added __str__ to all enums

--- tibiapy/enums.py
+++ tibiapy/enums.py
@@ -3,19 +3,24 @@ from enum import Enum
 __all__ = ('AccountStatus', 'HouseStatus', 'HouseType', 'PvpType', 'Sex', 'TransferType', 'Vocation', 'WorldLocation')
 
 
-class AccountStatus(Enum):
+class BaseEnum(Enum):
+    def __str__(self):
+        return self.value
+
+
+class AccountStatus(BaseEnum):
     """Possible account statuses."""
     FREE_ACCOUNT = "Free Account"
     PREMIUM_ACCOUNT = "Premium Account"
 
 
-class HouseStatus(Enum):
+class HouseStatus(BaseEnum):
     """Renting statuses of a house."""
     RENTED = "rented"
     AUCTIONED = "auctioned"
 
 
-class HouseType(Enum):
+class HouseType(BaseEnum):
     """
     The types of house available.
     """
@@ -23,7 +28,7 @@ class HouseType(Enum):
     GUILDHALL = "guildhall"
 
 
-class PvpType(Enum):
+class PvpType(BaseEnum):
     """The possible PvP types a World can have."""
     OPEN_PVP = "Open PvP"
     OPTIONAL_PVP = "Optional PvP"
@@ -32,20 +37,20 @@ class PvpType(Enum):
     HARDCORE_PVP = "Hardcore PvP"
 
 
-class Sex(Enum):
+class Sex(BaseEnum):
     """Character genders."""
     MALE = "male"
     FEMALE = "female"
 
 
-class TransferType(Enum):
+class TransferType(BaseEnum):
     """The possible special transfer restrictions a world may have."""
     REGULAR = "regular"  #: No special transfer restrictions.
     BLOCKED = "blocked"  #: Can't transfer to this world, but can transfer out of this world.
     LOCKED = "locked"  #: Can transfer to this world, but can't transfer out of this world.
 
 
-class Vocation(Enum):
+class Vocation(BaseEnum):
     """The possible vocation types."""
     NONE = "None"
     DRUID = "Druid"
@@ -58,7 +63,7 @@ class Vocation(Enum):
     MASTER_SORCERER = "Master Sorcerer"
 
 
-class WorldLocation(Enum):
+class WorldLocation(BaseEnum):
     """The possible physical locations for servers."""
     EUROPE = "Europe"
     NORTH_AMERICA = "North America"
