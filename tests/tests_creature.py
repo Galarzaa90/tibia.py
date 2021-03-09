e886d02d59d32e5cef6a37ef65d7e17427b02e6a
Allan Galarza <allan.galarza@gmail.com>
2021-03-08 17:10:35 -0700

    Added Creatures library parsing

--- tests/tests_creature.py
+++ tests/tests_creature.py
@@ -1,8 +1,10 @@
 import unittest
 
 from tests.tests_tibiapy import TestCommons
-from tibiapy import BoostedCreature, InvalidContent
+from tibiapy import BoostedCreature, InvalidContent, CreaturesSection, CreatureDetail
 
+FILE_CREATURE_SECTION = "library/creature_list.txt"
+FILE_CREATURE = "library/creature_animatedfeather.txt"
 
 class TestCreature(TestCommons, unittest.TestCase):
     # region Tibia.com Tests
@@ -19,4 +21,28 @@ class TestCreature(TestCommons, unittest.TestCase):
         with self.assertRaises(InvalidContent):
             BoostedCreature.from_content("<html><div><p>Nothing</p></div></html>")
 
+    def test_creature_section_from_content(self):
+        content = self.load_resource(FILE_CREATURE_SECTION)
+        creatures = CreaturesSection.from_content(content)
+
+        self.assertIsNotNone(creatures)
+        self.assertIsNotNone(creatures.boosted_creature)
+        self.assertEqual("Blood Crab", creatures.boosted_creature.name)
+        self.assertEqual("bloodcrab", creatures.boosted_creature.race)
+        self.assertIsNotNone(creatures.boosted_creature.image_url)
+        self.assertIsNotNone(creatures.boosted_creature.url)
+        self.assertEqual(536, len(creatures.creatures))
+        for creature in creatures.creatures:
+            with self.subTest(name=creature.name):
+                self.assertIsInstance(creature.name, str)
+                self.assertIsInstance(creature.race, str)
+
+    def test_creature_detail_from_content(self):
+        content = self.load_resource(FILE_CREATURE)
+        creature = CreatureDetail.from_content(content)
+
+        self.assertIsNotNone(creature)
+        self.assertEqual("Animated Feathers", creature.name)
+        self.assertEqual("animatedfeather", creature.race)
+        self.assertIsNotNone(creature.description)
     # endregion
--- tibiapy/creature.py
+++ tibiapy/creature.py
@@ -1,11 +1,17 @@
 import bs4
+import urllib.parse
 
 from tibiapy import abc
 from tibiapy.errors import InvalidContent
 
 __all__ = (
     "BoostedCreature",
+    "Creature",
+    "CreaturesSection",
+    "CreatureDetail",
 )
+
+from tibiapy.utils import parse_tibiacom_content, get_tibia_url
 
 BOOSTED_ALT = "Today's boosted creature: "
 
@@ -65,3 +71,123 @@ class BoostedCreature(abc.Serializable):
         except TypeError:
             raise InvalidContent("content is not from Tibia.com")
 
+
+class Creature(abc.Serializable):
+    """Represents a creature in the Library section.
+
+    Attributes
+    ----------
+    name: :class:`str`
+        The name of the creature, in plural.
+    race: :class:`str`
+        The internal name of the creature's race. Used for links and images."""
+
+    __slots__ = (
+        "name",
+        "race",
+    )
+
+    def __init__(self, name, race=None):
+        self.name = name
+        self.race = race
+
+    def __repr__(self):
+        return f"<{self.__class__.__name__} name={self.name!r} race={self.race!r}>"
+
+    @property
+    def url(self):
+        return self.get_url(self.race)
+
+    @property
+    def image_url(self):
+        return f"https://static.tibia.com/images/library/{self.race}.gif"
+
+    @classmethod
+    def get_url(cls, race):
+        return get_tibia_url("library", "creatures", race=race)
+
+
+class CreaturesSection(abc.Serializable):
+    """a
+
+    Attributes
+    ----------
+    boosted_creature: :class:`Creature`
+        The current boosted creature.
+    creatures: list of :class:`Creature`
+        The list of creatures in the library.
+    """
+
+    __slots__ = (
+        "boosted_creature",
+        "creatures",
+    )
+
+    def __init__(self, boosted_creature, creatures):
+        self.boosted_creature = boosted_creature
+        self.creatures = creatures or []
+
+    @classmethod
+    def from_content(cls, content):
+        parsed_content = parse_tibiacom_content(content)
+        boosted_creature_table = parsed_content.find("div", {"class": "TableContainer"})
+        boosted_creature_text = boosted_creature_table.find("div", {"class": "Text"})
+        if not boosted_creature_text or "Boosted" not in boosted_creature_text.text:
+            return None
+        boosted_creature_link = boosted_creature_table.find("a")
+        url = urllib.parse.urlparse(boosted_creature_link["href"])
+        query = urllib.parse.parse_qs(url.query)
+        boosted_creature = Creature(boosted_creature_link.text, query["race"][0])
+
+        list_table = parsed_content.find("div", style=lambda v: v and 'display: table' in v)
+        entries_container = list_table.find_all("div", style=lambda v: v and 'float: left' in v)
+        entries = []
+        for entry_container in entries_container:
+            name = entry_container.text.strip()
+            link = entry_container.find("a")
+            url = urllib.parse.urlparse(link["href"])
+            query = urllib.parse.parse_qs(url.query)
+            entries.append(Creature(name, query["race"][0]))
+        return cls(boosted_creature, entries)
+
+
+class CreatureDetail(abc.Serializable):
+    __slots__ = (
+        "name",
+        "race",
+        "description",
+        "hitpoints",
+        "experience"
+    )
+
+    def __init__(self, name, race):
+        self.name = name
+        self.race = race
+
+    def __repr__(self):
+        return f"<{self.__class__.__name__} name={self.name!r} race={self.race!r}>"
+
+    @classmethod
+    def get_url(cls, race):
+        return get_tibia_url("library", "creatures", race=race)
+
+    @classmethod
+    def from_content(cls, content):
+        parsed_content = parse_tibiacom_content(content)
+        pagination_container, content_container = \
+            parsed_content.find_all("div", style=lambda v: v and 'position: relative' in v)
+        title_container, description_container = content_container.find_all("div")
+        title = title_container.find("h2")
+        name = title.text.strip()
+
+        img = title_container.find("img")
+        img_url = img["src"]
+        race = img_url.split("/")[-1].replace(".gif", "")
+        creature = cls(name, race)
+
+        paragraph_tags = description_container.find_all("p")
+        paragraphs = [p.text for p in paragraph_tags]
+        creature.description = "\n".join(paragraphs[:-2])
+        hp_text = paragraphs[-2]
+        exp_text = paragraphs[-1]
+        return creature
