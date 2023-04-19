0f21eb9a13a0e8b065b36cdbf28041dc72cf752a
Allan Galarza <allan.galarza@gmail.com>
2023-04-18 19:38:33 -0700

    cleanup in spells models and builders

--- tibiapy/builders/spell.py
+++ tibiapy/builders/spell.py
@@ -1,43 +1,47 @@
-from typing import Optional, List
+from __future__ import annotations
 
-from tibiapy import VocationSpellFilter, SpellGroup, SpellSorting, SpellType
+from typing import Optional, List, TYPE_CHECKING
+
 from tibiapy.models import SpellEntry, SpellsSection, Spell, Rune
+
+if TYPE_CHECKING:
+    from tibiapy import VocationSpellFilter, SpellGroup, SpellSorting, SpellType
 
 
 class SpellSectionBuilder:
-    def __init__(self, **kwargs):
-        self._vocation = kwargs.get("vocation")
-        self._group = kwargs.get("group")
-        self._spell_type = kwargs.get("spell_type")
-        self._premium = kwargs.get("premium")
-        self._sort_by = kwargs.get("sort_by")
-        self._entries = kwargs.get("entries", [])
+    def __init__(self):
+        self._vocation = None
+        self._group = None
+        self._spell_type = None
+        self._premium = None
+        self._sort_by = None
+        self._entries = []
 
-    def vocation(self, vocation: Optional[VocationSpellFilter]) -> 'SpellSectionBuilder':
+    def vocation(self, vocation: Optional[VocationSpellFilter]):
         self._vocation = vocation
         return self
 
-    def group(self, group: Optional[SpellGroup]) -> 'SpellSectionBuilder':
+    def group(self, group: Optional[SpellGroup]):
         self._group = group
         return self
 
-    def spell_type(self, spell_type: Optional[VocationSpellFilter]) -> 'SpellSectionBuilder':
+    def spell_type(self, spell_type: Optional[VocationSpellFilter]):
         self._spell_type = spell_type
         return self
 
-    def premium(self, premium: Optional[bool]) -> 'SpellSectionBuilder':
+    def premium(self, premium: Optional[bool]):
         self._premium = premium
         return self
 
-    def sort_by(self, sort_by: SpellSorting) -> 'SpellSectionBuilder':
+    def sort_by(self, sort_by: SpellSorting):
         self._sort_by = sort_by
         return self
 
-    def entries(self, entries: List[SpellEntry]) -> 'SpellSectionBuilder':
+    def entries(self, entries: List[SpellEntry]):
         self._entries = entries
         return self
 
-    def add_entry(self, spell: SpellEntry) -> 'SpellSectionBuilder':
+    def add_entry(self, spell: SpellEntry):
         self._entries.append(spell)
         return self
 
@@ -53,50 +57,50 @@ class SpellSectionBuilder:
 
 
 class SpellEntryBuilder:
-    def __init__(self, **kwargs):
-        self._identifier = kwargs.get("identifier")
-        self._name = kwargs.get("name")
-        self._words = kwargs.get("words")
-        self._group = kwargs.get("group")
-        self._spell_type = kwargs.get("spell_type")
-        self._exp_level = kwargs.get("exp_level")
-        self._mana = kwargs.get("mana")
-        self._price = kwargs.get("price")
-        self._premium = kwargs.get("premium")
+    def __init__(self):
+        self._identifier = None
+        self._name = None
+        self._words = None
+        self._group = None
+        self._spell_type = None
+        self._exp_level = None
+        self._mana = None
+        self._price = None
+        self._premium = None
 
-    def identifier(self, identifier: str) -> 'SpellEntryBuilder':
+    def identifier(self, identifier: str):
         self._identifier = identifier
         return self
 
-    def name(self, name: str) -> 'SpellEntryBuilder':
+    def name(self, name: str):
         self._name = name
         return self
 
-    def words(self, words: str) -> 'SpellEntryBuilder':
+    def words(self, words: str):
         self._words = words
         return self
 
-    def group(self, group: SpellGroup) -> 'SpellEntryBuilder':
+    def group(self, group: SpellGroup):
         self._group = group
         return self
 
-    def spell_type(self, spell_type: SpellType) -> 'SpellEntryBuilder':
+    def spell_type(self, spell_type: SpellType):
         self._spell_type = spell_type
         return self
 
-    def exp_level(self, exp_level: int) -> 'SpellEntryBuilder':
+    def exp_level(self, exp_level: int):
         self._exp_level = exp_level
         return self
 
-    def mana(self, mana: Optional[int]) -> 'SpellEntryBuilder':
+    def mana(self, mana: Optional[int]):
         self._mana = mana
         return self
 
-    def price(self, price: int) -> 'SpellEntryBuilder':
+    def price(self, price: int):
         self._price = price
         return self
 
-    def premium(self, premium: bool) -> 'SpellEntryBuilder':
+    def premium(self, premium: bool):
         self._premium = premium
         return self
 
@@ -116,61 +120,61 @@ class SpellEntryBuilder:
 
 class SpellBuilder(SpellEntryBuilder):
 
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self._description = kwargs.get("description")
-        self._vocations = kwargs.get("vocations")
-        self._cooldown = kwargs.get("cooldown")
-        self._cooldown_group = kwargs.get("cooldown_group")
-        self._group_secondary = kwargs.get("group_secondary")
-        self._cooldown_group_secondary = kwargs.get("cooldown_group_secondary")
-        self._soul_points = kwargs.get("soul_points")
-        self._amount = kwargs.get("amount")
-        self._magic_type = kwargs.get("magic_type")
-        self._cities = kwargs.get("cities")
-        self._rune = kwargs.get("rune")
+    def __init__(self):
+        super().__init__()
+        self._description = None
+        self._vocations = None
+        self._cooldown = None
+        self._cooldown_group = None
+        self._group_secondary = None
+        self._cooldown_group_secondary = None
+        self._soul_points = None
+        self._amount = None
+        self._magic_type = None
+        self._cities = []
+        self._rune = None
 
-    def description(self, description: str) -> 'SpellEntryBuilder':
+    def description(self, description: str):
         self._description = description
         return self
 
-    def vocations(self, vocations: str) -> 'SpellEntryBuilder':
+    def vocations(self, vocations: str):
         self._vocations = vocations
         return self
 
-    def cooldown(self, cooldown: int) -> 'SpellEntryBuilder':
+    def cooldown(self, cooldown: int):
         self._cooldown = cooldown
         return self
 
-    def cooldown_group(self, cooldown_group: Optional[int]) -> 'SpellEntryBuilder':
+    def cooldown_group(self, cooldown_group: Optional[int]):
         self._cooldown_group = cooldown_group
         return self
 
-    def cooldown_group_secondary(self, cooldown_group_secondary: Optional[int]) -> 'SpellEntryBuilder':
+    def cooldown_group_secondary(self, cooldown_group_secondary: Optional[int]):
         self._cooldown_group_secondary = cooldown_group_secondary
         return self
 
-    def group_secondary(self, group_secondary: SpellGroup) -> 'SpellEntryBuilder':
+    def group_secondary(self, group_secondary: SpellGroup):
         self._group_secondary = group_secondary
         return self
 
-    def soul_points(self, soul_points: SpellType) -> 'SpellEntryBuilder':
+    def soul_points(self, soul_points: SpellType):
         self._soul_points = soul_points
         return self
 
-    def amount(self, amount: int) -> 'SpellEntryBuilder':
+    def amount(self, amount: int):
         self._amount = amount
         return self
 
-    def magic_type(self, magic_type: Optional[int]) -> 'SpellEntryBuilder':
+    def magic_type(self, magic_type: Optional[int]):
         self._magic_type = magic_type
         return self
 
-    def cities(self, cities: List[str]) -> 'SpellEntryBuilder':
+    def cities(self, cities: List[str]):
         self._cities = cities
         return self
 
-    def rune(self, rune: Rune) -> 'SpellEntryBuilder':
+    def rune(self, rune: Rune):
         self._rune = rune
         return self
 
@@ -200,40 +204,40 @@ class SpellBuilder(SpellEntryBuilder):
 
 
 class RuneBuilder:
-    def __init__(self, **kwargs):
-        self._name = kwargs.get("name")
-        self._vocations = kwargs.get("vocations")
-        self._group = kwargs.get("group")
-        self._exp_level = kwargs.get("exp_level")
-        self._mana = kwargs.get("mana")
-        self._magic_level = kwargs.get("magic_level")
-        self._magic_type = kwargs.get("magic_type")
+    def __init__(self):
+        self._name = None
+        self._vocations = None
+        self._group = None
+        self._exp_level = None
+        self._mana = None
+        self._magic_level = None
+        self._magic_type = None
 
-    def name(self, name: str) -> 'RuneBuilder':
+    def name(self, name: str):
         self._name = name
         return self
 
-    def vocations(self, vocations: List[str]) -> 'RuneBuilder':
+    def vocations(self, vocations: List[str]):
         self._vocations = vocations
         return self
 
-    def group(self, group: SpellGroup) -> 'RuneBuilder':
+    def group(self, group: SpellGroup):
         self._group = group
         return self
 
-    def exp_level(self, exp_level: int) -> 'RuneBuilder':
+    def exp_level(self, exp_level: int):
         self._exp_level = exp_level
         return self
 
-    def mana(self, mana: Optional[int]) -> 'RuneBuilder':
+    def mana(self, mana: Optional[int]):
         self._mana = mana
         return self
 
-    def magic_level(self, magic_level: int) -> 'RuneBuilder':
+    def magic_level(self, magic_level: int):
         self._magic_level = magic_level
         return self
 
-    def magic_type(self, magic_type: Optional[str]) -> 'RuneBuilder':
+    def magic_type(self, magic_type: Optional[str]):
         self._magic_type = magic_type
         return self
 
--- tibiapy/models/spell.py
+++ tibiapy/models/spell.py
@@ -6,7 +6,7 @@ from typing import List, Optional
 from pydantic import BaseModel
 
 from tibiapy import VocationSpellFilter, SpellGroup, SpellType, SpellSorting
-from tibiapy.urls import get_spells_section_url, get_spell_url
+from tibiapy.urls import get_spells_section_url, get_spell_url, get_static_file_url
 
 __all__ = (
     'SpellsSection',
@@ -14,7 +14,6 @@ __all__ = (
     'SpellEntry',
     'Rune'
 )
-
 
 
 class Rune(BaseModel):
@@ -49,9 +48,9 @@ class SpellEntry(BaseModel):
     """The cooldown group of the spell."""
     spell_type: SpellType
     """The type of the spell"""
-    exp_level: Optional[int] = None
+    exp_level: Optional[int]
     """The required level to cast the spell."""
-    mana: Optional[int] = None
+    mana: Optional[int]
     """The mana required to use the spell. If :obj:`None`, the mana cost is variable."""
     price: int
     """The price in gold coins to learn the spell."""
@@ -66,7 +65,7 @@ class SpellEntry(BaseModel):
     @property
     def image_url(self) -> str:
         """The URL to this spell's image."""
-        return f"https://static.tibia.com/images/library/{self.identifier}.png"
+        return get_static_file_url("images", "library", f"{self.identifier}.png")
 
 
 class Spell(SpellEntry):
@@ -78,17 +77,17 @@ class Spell(SpellEntry):
     """The vocations that can use this spell."""
     cooldown: int
     """The individual cooldown of this spell in seconds."""
-    cooldown_group: Optional[int] = None
+    cooldown_group: Optional[int]
     """The group cooldown of this spell in seconds."""
-    group_secondary: Optional[str] = None
+    group_secondary: Optional[str]
     """The secondary cooldown group of this spell, if any."""
-    cooldown_group_secondary: Optional[int] = None
+    cooldown_group_secondary: Optional[int]
     """The secondary cooldown of this spell in seconds."""
-    soul_points: Optional[int] = None
+    soul_points: Optional[int]
     """The number of soul points consumed by this spell. It will be :obj:`None` if not applicable."""
-    amount: Optional[int] = None
+    amount: Optional[int]
     """The amount of objects created by this spell. It will be :obj:`None` if not applicable."""
-    magic_type: Optional[str] = None
+    magic_type: Optional[str]
     """The type of magic of this spell. Influenced by specialized magic level attributes."""
     cities: List[str]
     """The cities where this spell can be learned."""
@@ -99,24 +98,27 @@ class Spell(SpellEntry):
 class SpellsSection(BaseModel):
     """The spells section in Tibia.com."""
 
-    vocation: Optional[VocationSpellFilter] = None
+    vocation: Optional[VocationSpellFilter]
     """The selected vocation filter. If :obj:`None`, spells for any vocation will be shown."""
-    group: Optional[SpellGroup] = None
+    group: Optional[SpellGroup]
     """The selected spell group to display. If :obj:`None`, spells for any group will be shown."""
-    spell_type: Optional[SpellType] = None
+    spell_type: Optional[SpellType]
     """The selected spell type to display. If :obj:`None`, spells for any type will be shown."""
-    premium: Optional[bool] = None
+    premium: Optional[bool]
     """The premium status to filter in. :obj:`True` to show only premium spells,
         :obj:`False` to show free account spells and :obj:`None` will show any spells."""
     sort_by: SpellSorting
     """The sorting order of the displayed spells."""
-    entries: List[SpellEntry] = []
+    entries: List[SpellEntry]
     """The spells matching the selected filters."""
 
     @property
     def url(self) -> str:
         """The URL to the spells section in Tibia.com."""
-        return get_spells_section_url(vocation=self.vocation, group=self.group, spell_type=self.spell_type, premium=self.premium,
-                            sort=self.sort_by)
-
-
+        return get_spells_section_url(
+            vocation=self.vocation,
+            group=self.group,
+            spell_type=self.spell_type,
+            premium=self.premium,
+            sort=self.sort_by
+        )
