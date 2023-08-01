fb0830220c2eb3c562319986ea1c7f8ce3d020a8
Allan Galarza <allan.galarza@gmail.com>
2023-08-01 07:20:03 -0700

    Fixed import order messed by organize imports

--- tibiapy/__init__.py
+++ tibiapy/__init__.py
@@ -1,28 +1,29 @@
 """Tibia.com parsing and fetching library."""
-__version__ = '5.6.1'
+__version__ = '5.6.1.post1'
 __author__ = 'Allan Galarza'
 __license__ = 'Apache-2.0 License'
 
 import logging
-from logging import NullHandler
 
 from tibiapy import abc, enums, utils
-from tibiapy.bazaar import *
+from tibiapy.house import *
 from tibiapy.character import *
-from tibiapy.client import *
 from tibiapy.creature import *
+from tibiapy.event import *
 from tibiapy.enums import *
 from tibiapy.errors import *
-from tibiapy.event import *
 from tibiapy.forum import *
 from tibiapy.guild import *
 from tibiapy.highscores import *
-from tibiapy.house import *
 from tibiapy.kill_statistics import *
 from tibiapy.leaderboard import *
 from tibiapy.news import *
-from tibiapy.spell import *
 from tibiapy.tournament import *
 from tibiapy.world import *
+from tibiapy.bazaar import *
+from tibiapy.spell import *
+from tibiapy.client import *
+
+from logging import NullHandler
 
 logging.getLogger(__name__).addHandler(NullHandler())
